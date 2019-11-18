f4acbc809f4b1ea308fb39d46bdd2db32ad05e0d
Omri Sarig <omri.sarig13@gmail.com>
2019-11-18 21:02:11 +0200

    Add abstract implementation of a question
    A question will be used to test the user on various words. There would
    be different questions to the different levels of the words.
    
    This commits add the abstract implementation of a question, with all the
    abstract and implemented function of it.
    
    This commit was done as part of issue #5

---
+++ Utils/POC/Test/Question/abstract_question.py
@@ -0,0 +1,133 @@
+"""
+The implementation of an abstract question in the project.
+
+File: abstract_question.py
+"""
+
+import abc
+
+
+class AbstractQuestion(abc.ABC):
+    """Implement an abstract class of question."""
+
+    RIGHT_ANSWER = 0
+    WRONG_ANSWER = 1
+    RIGHT_ANSWER_BUT_NOT_WANTED = 2
+
+    @abc.abstractmethod
+    def _get_answer_from_word(self, current_word):
+        """Get the answer from the current word.
+
+        :current_word: The word to get the answer from.
+        :returns: A string containing the answer from the word.
+
+        """
+
+    def _get_answer(self):
+        """Get the right answer for the question.
+
+        :returns: The right answer for the question
+
+        """
+        return self._get_answer_from_word(self._current_word)
+
+    @abc.abstractmethod
+    def _get_question_word(self):
+        """Get the word of the question.
+
+        :returns: Get the word the question is about.
+
+        """
+
+    def __init__(self, current_word, dictionary):
+        """Initialize the object with all its needed fields."""
+        abc.ABC.__init__(self)
+        self._current_word = current_word
+        self._dictionary = dictionary
+        self._right_answer = self._get_answer()
+
+    @abc.abstractmethod
+    def _get_question(self):
+        """Get the string representing the question.
+
+        :returns: A string with the question.
+
+        """
+
+    @abc.abstractmethod
+    def _check_answer(self, current_answer):
+        """Check the current answer got.
+
+        :current_answer: The answer got from the user.
+        :returns: One of the optional answers in this module (RIGHT_ANSWER,
+                  WRONG_ANSWER, RIGHT_ANSWER_BUT_NOT_WANTED)
+
+        """
+
+    def ask_question(self):
+        """Ask the user the current question.
+
+        :returns: True if the user has answered right, False otherwise.
+
+        :note: The question will update the word pair with the result.
+
+        """
+        # Print the question.
+        question = self._get_question()
+        print(question)
+
+        # Get answers until getting the right answer or a wrong answer.
+        answer = input("answer:")
+        current_answer_correctness = self._check_answer(answer)
+        while current_answer_correctness == self.RIGHT_ANSWER_BUT_NOT_WANTED:
+            print("Your answer is correct but not the wanted one. Try again.")
+            answer = input("answer:")
+            current_answer_correctness = self._check_answer(answer)
+
+        # Update the number of answers for the question and return the result.
+        if current_answer_correctness == self.RIGHT_ANSWER:
+            print("Correct answer!")
+            self._current_word.answered_right()
+            return True
+
+        print('Wrong answer! Correct one is "{}"'.format(self._get_answer()))
+        self._current_word.answered_wrong()
+        return False
+
+    def _check_native_word_answer(self, answer):
+        """Check if the answer is the right native word of the question.
+
+        :answer: The answer got from the user.
+        :returns: One of the optional answers in this module (RIGHT_ANSWER,
+                  WRONG_ANSWER, RIGHT_ANSWER_BUT_NOT_WANTED)
+
+        """
+        if answer == self._current_word.native_word:
+            return self.RIGHT_ANSWER
+
+        all_translations = self._dictionary.get_all_translations_to_native(
+            self._current_word.translated_word)
+
+        if answer in all_translations:
+            return self.RIGHT_ANSWER_BUT_NOT_WANTED
+
+        return self.WRONG_ANSWER
+
+    def _check_translated_word_answer(self, answer):
+        """Check if the answer is the right translated word of the question.
+
+        :answer: The answer got from the user.
+        :returns: One of the optional answers in this module (RIGHT_ANSWER,
+                  WRONG_ANSWER, RIGHT_ANSWER_BUT_NOT_WANTED)
+
+        """
+        if answer == self._current_word.translated_word:
+            return self.RIGHT_ANSWER
+
+        all_translations = self._dictionary.get_all_translations_to_native(
+            self._current_word.native_word)
+
+        if answer in all_translations:
+            return self.RIGHT_ANSWER_BUT_NOT_WANTED
+
+        return self.WRONG_ANSWER
