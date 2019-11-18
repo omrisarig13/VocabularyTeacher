b92aa9a122a4d418e776e51a5f054369027bbb68
Omri Sarig <omri.sarig13@gmail.com>
2019-11-18 21:11:11 +0200

    Add QuestionFactory class to the project
    A question factory is an abstract class that will be responsible on
    building the right questions for the different words.
    
    The questions for the different words will be chosen according to the
    known level of the Words.
    
    This commit was done as part of issue #5

---
+++ Utils/POC/Test/Question/question_factory.py
@@ -0,0 +1,42 @@
+"""
+A module responsible on creating the right question.
+
+The question will be determined according to the known level of the word in
+question.
+
+File: question_factory.py
+"""
+
+from Test.Question import translate_question
+from Test.Question import multiple_choice_question
+
+class QuestionFactory():
+    """A class used to create different questions."""
+
+    QUESTIONS_BY_LEVEL = {
+        0: multiple_choice_question.MultipleChoiceToNativeQuestion,
+        1: translate_question.TranslateToNativeQuestion,
+        2: multiple_choice_question.MultipleChoiceToLearnedQuestion,
+        3: translate_question.TranslateToLearnedQuestion,
+        4: translate_question.TranslateToLearnedQuestion
+    }
+
+    def __init__(self, dictionary):
+        """Initialize the question factory.
+
+        :dictionary: The dictionary to use when creating questions.
+
+        """
+        self._dictionary = dictionary
+
+    def get_question(self, word_for_question):
+        """Get the appropriate question object for the current word.
+
+        :word_for_question: The word for the question.
+        :returns: The appropriate question. It will be one of the
+                  implementation of AbstractQuestion class
+
+        """
+        word_level = word_for_question.word_known_level
+        return self.QUESTIONS_BY_LEVEL[word_level](word_for_question,
+                                                   self._dictionary)
