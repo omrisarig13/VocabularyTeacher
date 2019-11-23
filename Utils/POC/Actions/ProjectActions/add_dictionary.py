657f9a3bd5ab43231871c42facb5c5609b98fba2
Omri Sarig <omri.sarig13@gmail.com>
2019-11-23 17:56:27 +0200

    Change AddDictionaryAction to not get parameters
    Until now, the directory action got all the parameters in its
    command. Now, all the commands are being changed to get the parameters
    in itself, in order to change the menu to use the menu from consulemenu.
    
    The dictionary now uses the qprompt library to get the input
    interactively from the user.
    
    This commit was done as part of series of commits that will change all
    the interface of the program to be more user friendly. This will be done
    using two python libraries, consolemenu and qprompt.
    
    This commit was done as part of issue #5

--- Utils/POC/Actions/ProjectActions/add_dictionary.py
+++ Utils/POC/Actions/ProjectActions/add_dictionary.py
@@ -4,6 +4,8 @@ An action that will add a dictionary to the menu.
 File: add_dictionary.py
 """
 
+import qprompt
+
 import dictionary
 
 import menu
@@ -15,32 +17,24 @@ class AddDictionaryAction(menu_actions.BaseAction):
 
     COMMAND_NAME = "add_dictionary"
     HELP_TEXT = "Add a new dictionary to the menu."
-    ARGUMENTS = """
-            native_language - The native language in the dictionary.
-            learned_language - The languege to learn using the dictionary.
-    """
-    USAGE = "{command_name} native_language learned_language".format(
-        command_name=COMMAND_NAME)
+    ARGUMENTS = """None"""
+    USAGE = "{command_name}".format(command_name=COMMAND_NAME)
 
     def run_command(self, menu_context, *args, **kwargs):
         """Run the command that will add the dictionary to the menu.
 
         :menu_context: The context of the menu to add the dictionary to.
-        :*args: The special arguments for the command. It will include the
-                native language and the learned language of it.
         :returns: True to make the menu continue to run
 
         :Note: This functions adds the dictionary to the menu context.
         """
-        if len(args) != 2:
-            raise menu.InvalidCommand(
-                "Invalid number of arguments for command. Read the help")
-
         if menu_context.get("dictionary", None):
             raise menu.InvalidCommand("Menu already has a dictionary.")
 
-        native_language = args[0]
-        learned_language = args[1]
+        native_language = qprompt.ask_str(
+            "Insert the native language of the dictionary")
+        learned_language = qprompt.ask_str(
+            "Insert the learned language of the dictionary")
 
         new_dictionary = dictionary.Dictionary(native_language,
                                                learned_language)
