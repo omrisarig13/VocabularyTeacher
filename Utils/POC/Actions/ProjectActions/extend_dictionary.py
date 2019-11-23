ee364484c1433f04720d511235f76faf13f353b5
Omri Sarig <omri.sarig13@gmail.com>
2019-11-23 20:28:29 +0200

    Change ExtendDictionaryAction to not get parameters
    Change the action of ExtendDictionaryAction to get the parameters
    directly from the user, instead of getting it from the menu. Now,
    the action will ask the parameters from the user, getting it only
    after the command was run.
    
    The command now uses the qprompt library to get the input
    interactively from the user.
    
    This commit was done as part of series of commits that will change all
    the interface of the program to be more user friendly. This will be done
    using two python libraries, consolemenu and qprompt.
    
    This commit was done as part of issue #5

--- Utils/POC/Actions/ProjectActions/extend_dictionary.py
+++ Utils/POC/Actions/ProjectActions/extend_dictionary.py
@@ -4,6 +4,8 @@ Extend the current dictionary with more words loaded from another dictionary.
 File: load_dictionary.py
 """
 
+import qprompt
+
 import menu
 from Actions import menu_actions
 from Actions.ProjectActions import load_dictionary
@@ -14,29 +16,21 @@ class ExtendDictionaryAction(menu_actions.BaseAction):
 
     COMMAND_NAME = "extend_dictionary"
     HELP_TEXT = "Extend the dictionary to with another one."
-    ARGUMENTS = """
-            file_name - The name of the file with the new dictionary.
-    """
-    USAGE = "{command_name} file_name".format(command_name=COMMAND_NAME)
+    ARGUMENTS = """None"""
+    USAGE = "{command_name}".format(command_name=COMMAND_NAME)
 
     def run_command(self, menu_context, *args, **kwargs):
         """Extend the dictionary in the context with the new loaded one.
 
         :menu_context: The context of the menu.
-        :*args: The special arguments for the command. It will include the
-                name of the file to load the dictionary from.
         :returns: True to make the menu continue to run
 
         """
-        if len(args) != 1:
-            raise menu.InvalidCommand(
-                "Invalid number of arguments for command. Read the help")
-
         if menu_context.get("dictionary", None) is None:
             raise menu.InvalidCommand(
                 "Can't extend dictionary when there isn't one.")
 
-        file_name = args[0]
+        file_name = qprompt.ask_str("Insert the new name of the dictionary")
         dictionary = menu_context["dictionary"]
 
         load_action = load_dictionary.LoadDictionaryAction.load_dictionary_file
