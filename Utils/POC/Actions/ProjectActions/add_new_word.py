e4866eaa9207d84f3f18381786614d1c0d2ce681
Omri Sarig <omri.sarig13@gmail.com>
2019-11-23 18:38:09 +0200

    Change AddNewWordAction to not get parameters
    Change the action of GetNewWord to get the parameters directly from the
    user, instead of getting it from the menu. Now, the action will ask the
    parameters from the user, getting it only after the command was run.
    
    The command now uses the qprompt library to get the input
    interactively from the user.
    
    This commit was done as part of series of commits that will change all
    the interface of the program to be more user friendly. This will be done
    using two python libraries, consolemenu and qprompt.
    
    This commit was done as part of issue #5

--- Utils/POC/Actions/ProjectActions/add_new_word.py
+++ Utils/POC/Actions/ProjectActions/add_new_word.py
@@ -4,6 +4,8 @@ An action that will add a new word pair to the dictionary.
 File: add_new_word.py
 """
 
+import qprompt
+
 import word_couple
 
 from Actions import menu_actions
@@ -15,40 +17,27 @@ class AddNewWordAction(menu_actions.BaseAction):
 
     COMMAND_NAME = "add_new_word"
     HELP_TEXT = "Add the given new word to the dictionary."
-    ARGUMENTS = """
-            native_word - The word in the native language.
-            learned_word - The word in the learned language.
-            known_level - optional - a number representing the word known
-                                     level.
-    """
-    USAGE = "{command_name} native_word learned_word [known_level]".format(
-        command_name=COMMAND_NAME)
+    ARGUMENTS = """None"""
+    USAGE = "{command_name}".format(command_name=COMMAND_NAME)
 
     def run_command(self, menu_context, *args, **kwargs):
         """Run the command that will add the new word to the dictionary.
 
         :menu_context: The context of the menu to add the dictionary to.
-        :*args: The special arguments for the command. It will include the
-                new word in the native language, the new word in the learned
-                language and maybe the known level of the word.
         :returns: True to make the menu continue to run
 
         :Note: This functions adds the new word to the dictionary.
 
         """
-        if len(args) == 3:
-            known_level = args[2]
-        elif len(args) == 2:
-            known_level = 0
-        else:
-            raise menu.InvalidCommand(
-                "Invalid number of arguments for command. Read the help")
-
         if menu_context.get("dictionary", None) is None:
             raise menu.InvalidCommand("Menu does not have a dictionary")
 
-        native_word = args[0]
-        learned_word = args[1]
+        native_word = qprompt.ask_str("Insert the native word")
+        learned_word = qprompt.ask_str("Insert the learned word")
+        known_level = qprompt.ask_int(
+            "Insert the known level",
+            0,
+            vld=list(range(word_couple.WordCouple.MAX_LEVEL + 1)))
 
         new_word = word_couple.WordCouple(native_word, learned_word,
                                           known_level)
