d4e22d58ce781fdb1cc5fe7229a3943074862bd0
Omri Sarig <omri.sarig13@gmail.com>
2019-11-23 20:35:51 +0200

    Change TranlateToNativeDictionaryAction to not get parameters
    Change the action of TranlateToNativeDictionaryAction to get the
    parameters directly from the user, instead of getting it from the menu.
    Now, the action will ask the parameters from the user, getting it only
    after the command was run.
    
    The command now uses the qprompt library to get the input
    interactively from the user.
    
    This commit was done as part of series of commits that will change all
    the interface of the program to be more user friendly. This will be done
    using two python libraries, consolemenu and qprompt.
    
    This commit was done as part of issue #5

--- Utils/POC/Actions/ProjectActions/dictionary_native_translate.py
+++ Utils/POC/Actions/ProjectActions/dictionary_native_translate.py
@@ -4,23 +4,22 @@ Get the translate of the word to the native language by the dictionary.
 File: dictionary_native_translate.py
 """
 
+import qprompt
+
 import menu
 from Actions import menu_actions
 
 
 class TranlateToNativeDictionaryAction(menu_actions.BaseAction):
-    """An action that prints the translations of a word to native languege."""
+    """An action that prints the translations of a word to native language."""
 
     COMMAND_NAME = "translate_to_native_dictionary"
     HELP_TEXT = """
         Translate the word to the native language using the dictionary.
         Get all the translations of the given word.
     """
-    ARGUMENTS = """
-            word_to_translate - The word to translate to the native language.
-    """
-    USAGE = "{command_name} word_to_translate".format(
-        command_name=COMMAND_NAME)
+    ARGUMENTS = """None"""
+    USAGE = "{command_name}".format(command_name=COMMAND_NAME)
 
     def run_command(self, menu_context, *args, **kwargs):
         """Print all the translation of a given word.
@@ -31,17 +30,14 @@ class TranlateToNativeDictionaryAction(menu_actions.BaseAction):
         :returns: True to make the menu continue to run
 
         """
-        if len(args) != 1:
-            raise menu.InvalidCommand(
-                "Invalid number of arguments for command. Read the help")
-
         if menu_context.get("dictionary", None) is None:
             raise menu.InvalidCommand(
                 "Can't translate without dictionary.")
 
-        word_to_translate = args[0]
+        word_to_translate = qprompt.ask_str("Insert the word to translate")
         dictionary = menu_context["dictionary"]
 
         print(dictionary.get_all_translations_to_native(word_to_translate))
 
+        qprompt.pause()
         return True
